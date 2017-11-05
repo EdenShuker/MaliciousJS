954400c156cbad689cfb7286ac043dd4b8051401
Eden Shuker <edenshuker1997@gmail.com>
2017-11-05 18:36:18 +0200

    video example code

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var index = require('./routes/index');
-var users = require('./routes/users');
+// var index = require('./routes/index');
+// var users = require('./routes/users');
 
 var app = express();
 
@@ -22,9 +22,25 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
-app.use('/users', users);
+// app.use('/', index);
+// app.use('/users', users);
 
+
+// the main page
+app.get('/', function(req,res){
+    res.render('index', {title: 'Eden' });
+});
+
+// the shout JSON endpoint
+app.get('/shout', function (req,res) {
+    var input = req.query.text;
+    //res.send('input: "'+input+'"');
+    res.json({result:input});
+});
+
+// app.get('/', (req,res)=>{
+//     res.render('index');
+// });
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
@@ -42,6 +58,8 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+
 
 module.exports = app;
 
